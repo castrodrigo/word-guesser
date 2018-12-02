d434ee22f6b59ffc01e1348a21d962320f7b62a8
castrodrigo <castrodrigo@yahoo.com.br>
2018-12-02 17:43:14 +0100

    Move action tests to integrations concept

--- src/actions/index.test.js
+++
@@ -1,9 +0,0 @@
-import { actionTypes, correctGuess } from "./index";
-
-describe("Correct Guess", () => {
-  test("returns an action with type 'CORRECT_GUESS'", () => {
-    const action = correctGuess();
-
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-  });
-});
